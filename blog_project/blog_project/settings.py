7c267a9b08776e52a8bd02171a2915932101496e
Michael Dediu-whealey <whealey22@gmail.com>
2018-05-20 18:37:45 -0700

    heroku

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '&ywrfxa3e386&$2)8-_*)z#uw82_v9&#9c8e(b48a0kj3@z*wi'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
